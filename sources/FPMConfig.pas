602ff9d5011aa641bb3a85e803694da5772420fc
Ryan Joseph <genericptr@gmail.com>
2020-10-20 10:40:45 -0600

    some options now properly override in targets

--- sources/FPMConfig.pas
+++ sources/FPMConfig.pas
@@ -12,20 +12,26 @@ type
   TFPMConfig = class
     private const
       kConfigFileName = 'fpconfig.toml';
+    private type
+      TOptions = record
+        compiler: string;     // path to compiler
+        main: string;         // path to program
+        executable: string;   // -o
+        output: string;       // -FU
+      end;
     private
       config: TTOMLDocument;
       settings: TTOMLTable;
       variables: TTOMLTable;
-      executable: string;
+      options: TOptions;
       m_target: TFPMTarget;
       function GetTarget: TFPMTarget;
       function GetCommandLine: string;
       function GetCommandLineForTable(table: TTOMLTable): string;
-      function GetCompiler: string;
       function ExpandValue(data: TTOMLData): string; inline;
       function ExpandPath(data: TTOMLData): string; inline;
       function ReplaceVariables(const s: string): string;
-      function ArrayToFlags(flag: ShortString; data: TTOMLArray; expandPaths: boolean = true): string;
+      function ArrayToFlags(flag: ShortString; data: TTOMLArray): string;
       procedure FatalError(message: string);
       procedure PrintStats;
     public
@@ -37,89 +43,7 @@ type
 
 implementation
 
-{
-  Free Pascal target is an Intel 80x86 or compatible (16 and 32 bit).
-  CPU87 Free Pascal target is an Intel 80x86 or compatible (16 and 32 bit).
-  CPU386  Free Pascal target is an Intel 80386 or later.
-  CPUI386 Free Pascal target is an Intel 80386 or later.
-  CPU68K  Free Pascal target is a Motorola 680x0 or compatible.
-  CPUM68K Free Pascal target is a Motorola 680x0 or compatible.
-  CPUM68020 Free Pascal target is a Motorola 68020 or later.
-  CPU68 Free Pascal target is a Motorola 680x0 or compatible.
-  CPUSPARC32  Free Pascal target is a SPARC v7 or compatible.
-  CPUSPARC  Free Pascal target is a SPARC v7 or compatible.
-  CPUALPHA  Free Pascal target is an Alpha AXP or compatible.
-  CPUPOWERPC  Free Pascal target is a 32-bit or 64-bit PowerPC or compatible.
-  CPUPOWERPC32  Free Pascal target is a 32-bit PowerPC or compatible.
-  CPUPOWERPC64  Free Pascal target is a 64-bit PowerPC or compatible.
-  CPUX86_64 Free Pascal target is a AMD64 or Intel 64-bit processor.
-  CPUAMD64  Free Pascal target is a AMD64 or Intel 64-bit processor.
-  CPUX64  Free Pascal target is a AMD64 or Intel 64-bit processor
-  CPUIA64 Free Pascal target is a Intel itanium 64-bit processor.
-  CPUARM  Free Pascal target is an ARM 32-bit processor.
-  CPUAVR  Free Pascal target is an AVR 16-bit processor.
-  CPU16 Free Pascal target is a 16-bit CPU.
-  CPU32 Free Pascal target is a 32-bit CPU.
-  CPU64 Free Pascal target is a 64-bit CPU.
-}
-
-{$if defined(CPUPOWERPC32)}
-  {$define TARGET_CPU_PPC}
-  {$undef TARGET_CPU_PPC64}
-  {$undef TARGET_CPU_X86}
-  {$undef TARGET_CPU_X86_64}
-  {$undef TARGET_CPU_ARM}
-  {$define TARGET_OS_MAC}
-  {$undef TARGET_OS_IPHONE}
-  {$undef TARGET_IPHONE_SIMULATOR}
-  {$undef TARGET_RT_64_BIT}
-{$elseif defined(CPUPOWERPC64)}
-  {$undef TARGET_CPU_PPC}
-  {$define TARGET_CPU_PPC64}
-  {$undef TARGET_CPU_X86}
-  {$undef TARGET_CPU_X86_64}
-  {$undef TARGET_CPU_ARM}
-  {$define TARGET_OS_MAC}
-  {$undef TARGET_OS_IPHONE}
-  {$undef TARGET_IPHONE_SIMULATOR}
-  {$define TARGET_RT_64_BIT}
-{$elseif defined(CPUI386)}
-  {$undef TARGET_CPU_PPC}
-  {$undef TARGET_CPU_PPC64}
-  {$define TARGET_CPU_X86}
-  {$undef TARGET_CPU_X86_64}
-  {$undef TARGET_CPU_ARM}
-  {$undef TARGET_RT_64_BIT}
-  {$if defined(IPHONESIM)}
-    {$undef TARGET_OS_MAC}
-    {$define TARGET_OS_IPHONE}
-    {$define TARGET_IPHONE_SIMULATOR}
-  {$else}
-    {$define TARGET_OS_MAC}
-    {$undef TARGET_OS_IPHONE}
-    {$undef TARGET_IPHONE_SIMULATOR}
-  {$endif}
-{$elseif defined(CPUX86_64)}
-  {$undef TARGET_CPU_PPC}
-  {$undef TARGET_CPU_PPC64}
-  {$undef TARGET_CPU_X86}
-  {$define TARGET_CPU_X86_64}
-  {$undef TARGET_CPU_ARM}
-  {$define TARGET_OS_MAC}
-  {$undef TARGET_OS_IPHONE}
-  {$undef TARGET_IPHONE_SIMULATOR}
-  {$define TARGET_RT_64_BIT}
-{$elseif defined(CPUARM)}
-  {$undef TARGET_CPU_PPC}
-  {$undef TARGET_CPU_PPC64}
-  {$undef TARGET_CPU_X86}
-  {$undef TARGET_CPU_X86_64}
-  {$define TARGET_CPU_ARM}
-  {$undef TARGET_OS_MAC}
-  {$define TARGET_OS_IPHONE}
-  {$undef TARGET_IPHONE_SIMULATOR}
-  {$undef TARGET_RT_64_BIT}
-{$endif}
+{$include target.inc}
 
 // TODO: we need this for packages also. maybe make a TFPMVariables class
 function TFPMConfig.ReplaceVariables(const s: string): string;
@@ -144,7 +68,7 @@ begin
   result := ExpandFileName(result);
 end;
 
-function TFPMConfig.ArrayToFlags(flag: ShortString; data: TTOMLArray; expandPaths: boolean = true): string;
+function TFPMConfig.ArrayToFlags(flag: ShortString; data: TTOMLArray): string;
 var
   value: TTOMLData;
   path: string;
@@ -154,24 +78,19 @@ begin
   result := '';
   for value in data do
     begin
-      if expandPaths then
-        begin
-          path := Trim(ExpandPath(value));
-          if not DirectoryExists(path) then
-            FatalError('Directory "'+path+'" for flag '+flag+' doesn''t exist');
-          result += flag+path;
-        end
-      else
-        result += flag+Trim(ExpandValue(value));
-      result += ' ';
+      path := Trim(ExpandPath(value));
+      if not DirectoryExists(path) then
+        FatalError('Directory "'+path+'" for flag '+flag+' doesn''t exist');
+      result += flag+path+' ';
     end;
 end;
 
 function TFPMConfig.GetCommandLineForTable(table: TTOMLTable): string;
 var
+  value: TTOMLData;
+  list: TTOMLArray;
   path: string;
 begin
-  // TODO: how do we deal with duplicates now? we need flag struct I guess
   result := '';
 
   // paths
@@ -181,33 +100,33 @@ begin
   result += ArrayToFlags('-Ff', table.Find('frameworkPaths') as TTOMLArray);
 
   // general options
-  result += ArrayToFlags('', table.Find('options') as TTOMLArray, false);
+  list := table.Find('options') as TTOMLArray;
+  if list <> nil then
+    for value in list do
+      result += Trim(ExpandValue(value))+' ';
 
   // output
   if table.Contains('output') then
-    begin
-      path := ExpandPath(table['output']);
-      if not DirectoryExists(path) then
-        ForceDirectories(path);
-      if not DirectoryExists(path) then
-        FatalError('Output directory '+path+' doesn''t exist');
-      result += '-FU'+path+' ';
-    end;
+    options.output := ExpandPath(table['output']);
 
   // executable
   if table.Contains('executable') then
-    begin
-      executable := ExpandPath(table['executable']);
-      result += '-o'+executable+' ';
-    end;
+    options.executable := ExpandPath(table['executable']);
+
+  // compiler
+  if table.Contains('compiler') then
+    options.compiler := ExpandPath(table['compiler']);
+
+  // program
+  if table.Contains('program') then
+    options.main := ExpandPath(table['program']);
 end;
 
 function TFPMConfig.GetCommandLine: string;
 var
   table: TTOMLTable;
 begin
-  result := ExpandPath(settings['program']);
-  result += ' ';
+  result := '';
 
   // base
   result += GetCommandLineForTable(settings);
@@ -222,6 +141,10 @@ begin
       table := config['configuration'][string(settings['configuration'])] as TTOMLTable;
       result += GetCommandLineForTable(table);
     end;
+
+  result += '-FU'+options.output+' ';
+  result += '-o'+options.executable+' ';
+  result += options.main+' ';
 end;
 
 function TFPMConfig.GetTarget: TFPMTarget;
@@ -240,25 +163,6 @@ begin
   result := m_target;
 end;
 
-function TFPMConfig.GetCompiler: string;
-begin
-  if settings.Contains('compiler') then
-    result := ExpandPath(settings['compiler'])
-  else
-    begin
-      {$if defined(DARWIN)}
-      result := '/usr/local/bin/fpc';
-      {$elseif defined(WINDOWS)}
-      // TODO: default path?
-      result := 'fpc';
-      {$elseif defined(LINUX)}
-      // TODO: default path?
-      result := 'fpc';
-      {$endif}
-    end;
-end;
-
-
 procedure TFPMConfig.PrintStats; 
 begin
   // settings.GetAsString('target', 'none')
@@ -271,16 +175,34 @@ end;
 
 function TFPMConfig.Execute: integer;
 var
-  path, commandLine: string;
+  commandLine: string;
 begin
-  path := GetCompiler;
+
+  // default compiler paths
+  {$if defined(DARWIN)}
+  options.compiler := '/usr/local/bin/fpc';
+  {$elseif defined(WINDOWS)}
+  // TODO: default path?
+  options.compiler := 'fpc';
+  {$elseif defined(LINUX)}
+  // TODO: default path?
+  options.compiler := 'fpc';
+  {$endif}
+
   commandLine := GetCommandLine;
   PrintStats;
-  writeln(path, ' ', commandLine);
+  writeln(options.compiler, ' ', commandLine);
+
+  // create output directories
+  if not DirectoryExists(options.output) then
+    ForceDirectories(options.output);
+  if not DirectoryExists(options.output) then
+    FatalError('Output directory '+options.output+' doesn''t exist');
+
   if target <> nil then
     target.RunScripts;
-  //halt;
-  result := ExecuteProcess(path, commandLine, []);
+
+  result := ExecuteProcess(options.compiler, commandLine, []);
 end;
 
 procedure TFPMConfig.FatalError(message: string);
